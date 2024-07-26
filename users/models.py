9f7feddc1667572aa2605faf5c16ae4c020bf558
Erik <erik.agayan1@gmail.com>
2024-07-26 18:44:28 +0300

    Add new comments in models.py

--- users/models.py
+++ users/models.py
@@ -4,16 +4,19 @@ from django.utils.translation import gettext as _
 
 
 class UserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
+    """Model manager for creating users without using the username field"""
 
+    # specifies that this manager should be used for migrations
     use_in_migrations = True
 
     def _create_user(self, email, username, password, **extra_fields):
-        """Create and save a User with the given email, username and password."""
+        """Private method, Create and save a User with the given email, username and password."""
+
         if not email:
             raise ValueError("The given email must be set")
         if not username:
             raise ValueError("The given username must be set")
+
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
@@ -22,25 +25,41 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, username, password=None, **extra_fields):
         """Create and save a regular User with the given email, username and password."""
+
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
+
+        # Calls the _create_user private method to create and save a user.
         return self._create_user(email, username, password, **extra_fields)
 
     def create_superuser(self, email, username, password, **extra_fields):
         """Create and save a SuperUser with the given email, username and password."""
+
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        # ... проверки на is_staff и is_superuser ...
+
+        # Calls the _create_user private method to create and save a superuser.
         return self._create_user(email, username, password, **extra_fields)
 
 
 class User(AbstractUser):
+    """User model"""
+
     is_moderator = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     email = models.EmailField(_("email address"), unique=True)
     username = models.CharField(_("username"), unique=True, max_length=150)
 
+    # Email for auth
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
+    # For this model you should use the custom UserManager
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
