fff8db6d8b5f0ca571f56ff0642bf2a7e01c4fd6
Erik <erik.agayan1@gmail.com>
2024-07-29 17:12:11 +0300

    Change serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -3,10 +3,13 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 
 
+User = get_user_model()
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = ("id", "username", "email", "password")
+        model = User
+        fields = ("id", "username", "email", "password", "is_moderator", "is_manager")
         extra_kwargs = {"password": {"write_only": True}}
 
     @staticmethod
@@ -15,19 +18,28 @@ class UserSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
-        user = get_user_model().objects.create(
-            username=validated_data["username"], email=validated_data["email"]
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
+        try:
+            user = User.objects.create_user(
+                username=validated_data["username"],
+                email=validated_data["email"],
+                password=validated_data["password"],
+                is_moderator=validated_data.get("is_moderator", False),
+                is_manager=validated_data.get("is_manager", False)
+            )
+            return user
+        except Exception as e:
+            raise serializers.ValidationError({"detail": str(e)})
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
-        user = super().update(instance, validated_data)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
 
         if password:
-            user.set_password(password)
-            user.save()
+            instance.set_password(password)
+        try:
+            instance.save()
+        except Exception as e:
+            raise serializers.ValidationError({"detail": str(e)})
 
-        return user
+        return instance
