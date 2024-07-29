6a7518d1cc2f857ce3b96403e2a44b686ea95d4c
Erik <erik.agayan1@gmail.com>
2024-07-29 18:45:56 +0300

    New README.md

--- users/serializers.py
+++ users/serializers.py
@@ -9,8 +9,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "email", "password",
-                  "is_moderator", "is_manager")
+        fields = ("id", "username", "email", "password")
         extra_kwargs = {"password": {"write_only": True}}
 
     @staticmethod
