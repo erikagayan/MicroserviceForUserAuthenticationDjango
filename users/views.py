2db3e87a39bc98831c23ebdb47c2a14070170f18
Erik <erik.agayan1@gmail.com>
2024-07-29 17:35:34 +0300

    Changes in views.py

--- users/views.py
+++ users/views.py
@@ -1,24 +1,27 @@
 from rest_framework import generics
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.views import TokenObtainPairView
-
 from users.serializers import UserSerializer
 from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
     serializer_class = UserSerializer
 
 
 class CreateTokenView(TokenObtainPairView):
+    """Create a new token for user"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
     serializer_class = UserSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    # return auth user
     def get_object(self):
         return self.request.user
