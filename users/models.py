45514931939b75ec7d1ef0d4d2368a6a3007b19d
erikagayan <erik.agayan1@gmail.com>
2026-08-23 15:50:25 +0200

    Remove unnecessary imports

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
